a0b0ffd624bb28ab44d7439411947bda3202cae8
pavani <muthyalapavanireddy1997@gmail.com>
2025-06-15 20:00:52 +0530

    Setup of the Framework

---
+++ src/main/java/org/example/pojos/BookingResponse.java
@@ -0,0 +1,33 @@
+
+package org.example.ex_010_PayloadManagement.gson;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.example.ex_010_PayloadManagement.ai_03.Booking;
+
+public class BookingResponse {
+
+    @SerializedName("bookingid")
+    @Expose
+    private Integer bookingid;
+    @SerializedName("booking")
+    @Expose
+    private Booking booking;
+
+    public Integer getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(Integer bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+}
